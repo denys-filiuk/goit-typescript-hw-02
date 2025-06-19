6c608deb8d1d70996625c0864b56eb96f7cbc0fa
denys-filiuk <filiuk_dp@ukr.net>
2025-06-19 16:02:47 +0300

    fix vulnerabilities

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,6 +1,7 @@
+import { ReactElement } from "react";
 import { ClipLoader } from "react-spinners";
 
-export default function Loader(): JSX.Element {
+export default function Loader(): ReactElement {
   return (
     <div>
       <ClipLoader size={50} color="#3f51b5" />
